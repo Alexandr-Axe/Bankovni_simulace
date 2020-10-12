166dcfa0ff567327ac7e6c27a6e6bb3d4ce85b6f
Alexandr-Axe <58432418+Alexandr-Axe@users.noreply.github.com>
2020-10-12 21:02:26 +0200

    Add files via upload

--- Bankovni_Simulace_MVOP/MainWindow.xaml.cs
+++ Bankovni_Simulace_MVOP/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace Bankovni_Simulace_MVOP
         {
             InitializeComponent();
         }
+        //public List<Ucet> NUcet = new List<Ucet>();
+        public List<Sporici> SUcet = new List<Sporici>();
+        public List<Studentsky> StUcet = new List<Studentsky>();
+        public List<Kreditni> KUcet = new List<Kreditni>();
+        VytvoritUcet VU = new VytvoritUcet();
+        private void ShowBAccount_Click(object sender, RoutedEventArgs e)
+        {
+            MojeUcty MU = new MojeUcty();
+            MU.Show();
+            Close();
+        }
+
+        private void CreateDeposit_Click(object sender, RoutedEventArgs e)
+        {
+            VU.Show();
+            Close();
+        }
     }
+   
 }
---
+++ Bankovni_Simulace_MVOP/MojeUcty.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankovni_Simulace_MVOP
+{
+    /// <summary>
+    /// Interakční logika pro MojeUcty.xaml
+    /// </summary>
+    public partial class MojeUcty : Window
+    {
+        MainWindow MW = new MainWindow();
+        public MojeUcty()
+        {
+            InitializeComponent();
+            //sporici.Content = MW.SUcet[0].ToString(); Z nějakého důvodu nefunguje
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MW.Show();
+            Close();
+        }
+    }
+}
